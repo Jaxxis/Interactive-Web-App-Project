f390ba19ce457e9254189c73146da1ee1785dd35
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-04-26 10:09:41 +1000

    accessed api using javascript

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const pokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
 
 pokemon
-.then(res => res.json())
-.then(pokeArray => console.log(pokeArray))
+    .then(res => res.json())
+    .then(({results}) => {
+        results.forEach(pokemon => renderPokeCard(pokemon));
+    })
+
+
+//Render PokeCards
+function renderPokeCard({url}) {
+    fetch(url)
+        .then(res => res.json())
+        .then(pokemon => {
+            const pokeCards = document.querySelector('#cards')
+            pokeCards.innerHTML = pokeCards.innerHTML + `
+        <li>
+            <div class="poke-card">
+                <h4>${pokemon.name}</h4>
+                <img src= ${pokemon.sprites.front_default}>
+            </div>    
+        </li>`
+            })
+
+}
+
+
+//Render Top 3 list
+
+
+//Render Bottom 3 list
+
+
