a44f1d2b274bcb9f7444509d49930479c6b44a42
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-05-05 17:20:53 +1000

    fix JS search button function

--- HP.js
+++ HP.js
@@ -1,132 +1,62 @@
-// Creat PokeArray
+//Search Button 
+const charactersList = document.getElementById('cards');
+const searchBar = document.getElementById('search-bar');
+let hpCharacters = [];
 
-const characterArray = []
+searchBar.addEventListener('keyup', (e) => {
+    const input = e.target.value.toLowerCase();
 
+    const filteredCharacters = hpCharacters.slice(0, 25).filter((character) => {
+        return (
+            character.name.toLowerCase().includes(input)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
 
-//Async fetch request
-async function getCharacters() {
-    await fetch('https://hp-api.onrender.com/api/characters')
-    .then (res => res.json())
-    .then (data =>
-    data.forEach(value => characterArray.push(value))
-)}
+//Fetch Characters
+const loadCharacters = async () => {
+    const res = await fetch('https://hp-api.onrender.com/api/characters');
+    hpCharacters = await res.json();
+    displayCharacters(hpCharacters.slice(0, 25));
 
-// Call fetch Function
+};
 
-console.log(characterArray)
+//Amend DOM
 
-
-
-//Render HP Characters
-
-function renderCharacter(characters) {
-            const character = document.querySelector('#cards')
-            character.innerHTML = character.innerHTML + `
-        <li>
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li>
             <div class="character-card">
-                <img class="character-images" src=${characters.image} >
-                <h4 class="character-names">${characters.name}</h4>
-                <h4 class="character-houses">${characters.house}</h4>
+                <h2 class="character-name">${character.name}</h2>
+                <p class="character-house">House: ${character.house}</p>
+                <img class="character-images" src="${character.image}"></img>
                 <div id="vote-btns">
-                    <button class="like-btn"> Like </button>
+                     <button class="like-btn"> Like </button>
                     <button class="dislike-btn"> Dislike </button>
-                </div>   
-            </div>    
-        </li>`
-        }
+                </div>
+            </div>   
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
 
+loadCharacters();
 
+//Like Button Event Listener
 
-
-async function render() {
-    await getCharacters()
-    characterArray.forEach(characters => renderCharacter(characters))
+const likeButton = document.getElementsByClassName('like-btn')
+console.log(likeButton)
+for (button of likeButton) {
+    button.addEventListener('click', event => {
+        const favoriteCharacter = event.target.parentNode;
+        console.log(favoriteCharacter)
+    })
 }
 
-render()
 
-
-
-
-
-
-
-// Search Bar    
-const searchBar = document.querySelector("#search-bar");
-searchBar.addEventListener('keyup', (e) => {
-    const input = e.target.value
-    const filteredCharaters = characterArray.filter(character =>{
-       return character.name.includes(input)
-    })
-    console.log(filteredCharaters)
-    renderCharacter(filteredCharaters)
-})
-
-
-
-// // Create array to store likes
-
-// const likes = []
-
-// // //Create like and dislike buttons
-
-// document.querySelectorAll('.like-btn, .dislike-btn').forEach(button => {
-//     button.addEventListener('click', function (event) {
-//         const pokemonName = event.target.parentElement.parentElement.querySelector('#cards')
-//         if (event.target.classList.contains('like-btn')) {
-//             // Increment the like count for the Pokemon
-//             if (likes[pokemonName]) {
-//               likes[pokemonName]++;
-//             } else {
-//               likes[pokemonName] = 1;
-//             }
-//           } else {
-//             // Decrement the like count for the Pokemon
-//             if (likes[pokemonName]) {
-//               likes[pokemonName]--;
-//             } else {
-//               likes[pokemonName] = -1;
-//             }
-//           }
-//           // Re-render the top and bottom 3 lists
-//           renderTop3();
-//           renderBottom3();
-//         });
-//       });
-
-// //ATTEMPT 1 at making likes button 
-// const favourites = []
-
-// const likeButton = document.querySelectorAll('.like-btn')
-
-// likeButton.forEach(button => {
-//     button.addEventListener('click', event => {
-//     const favoriteCard = event.target.parentNode.parentNode;
-//     const favoriteCardIndex = favourites.findIndex((e) => e.favoriteCard === favoriteCard)
-
-//     if(favoriteCardIndex !== -1){
-//         favourites[favoriteCardIndex].count += 1;
-//     } else {
-//         favourites.push({ favoriteCard, count: 1})
-//     }
-
-//     })
-// })
-
-// console.log(favourites)
-
-// //Render Top 3 list
-// function renderTopThree() {
-//     const topThree = document.querySelector("#top-three-list")
-//     topThree.innerHTML = topThree.innerHTML + `
-// <li>
-//     <div class="poke-card">
-//         <h4>${pokemon.name}</h4>
-//         <img src= ${pokemon.sprites.front_shiny}>
-//             <p></p>
-//         </div>   
-//     </div>    
-// </li>`
-// }
-
-// // Render Bottom 3 list
+// Dislike Button Event Listener
--- test.js
+++ test.js
@@ -1,4 +1,39 @@
-fetch('https://hp-api.onrender.com/api/characters')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const charactersList = document.getElementById('cards');
+const searchBar = document.getElementById('search-bar');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const input = e.target.value.toLowerCase();
+    
+    const filteredCharacters = hpCharacters.slice(0, 24).filter((character) => {
+        return (
+            character.name.toLowerCase().includes(input)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+        const res = await fetch('https://hp-api.onrender.com/api/characters');
+        hpCharacters = await res.json();
+        console.log(hpCharacters)
+        displayCharacters(hpCharacters.slice(0, 24));
+  
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
